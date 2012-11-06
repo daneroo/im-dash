f79ff52ee0b6c8c7fd498970c7756804efc5acd8
Daniel Lauzon <daniel.lauzon@gmail.com>
2012-11-06 13:36:30 -0500

    tone down logging

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ app.use(express.static(__dirname + '/public'));
 var server = require('http').createServer(app);
 
 var io = require('socket.io').listen(server,ioOpts);
+io.configure(function(){
+  io.set('log level', 1);
+});
+io.configure('production', function(){
+  // io.set('log level', 0);
+});
 
 console.log("Listening on http://"+host+":"+port);
 server.listen(port);
@@ -27,12 +33,12 @@ io.sockets.on('connection', function (socket) {
   },5000)
   
   socket.on('metric', function (data) {
-    console.log('metric:',data);
+    // console.log('metric:',data);
 	  socket.broadcast.emit('metric',data);
   });
   
   socket.on('control', function (data) {
-    console.log('control:',data);
+    // console.log('control:',data);
 	  socket.broadcast.emit('control',data);
   });
   
